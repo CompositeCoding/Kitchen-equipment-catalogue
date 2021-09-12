4a727a1ba044ed90cd6c90bbbedc5d34ef746f8d
compositecoding <compositecoding@outlook.com>
2021-09-12 20:55:33 +0200

    bug fixes

--- VanderVliet/settings.py
+++ VanderVliet/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST = ''
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = '587'
